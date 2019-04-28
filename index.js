e427f6401f8e7cf6848d0f388e93882c8b6a5ceb
Marcelo Autran <mrautran@gmail.com>
2019-04-27 19:14:08 -0700

    Initialized dependencies and created simple express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(4000, function(){
+  console.log('listening on port 4000');
+});
